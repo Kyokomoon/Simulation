d932fbe30f1b0cb417bca17438cdfebe4f3e9b9e
Кулаков Кирилл Вячеславович <kiril2003140@yandex.ru>
2023-02-26 00:31:33 +0700

    refactor lab2

--- lab3.cpp
+++ lab3.cpp
@@ -1,97 +1,187 @@
-#include<stdio.h> //работоспособность проверенна на Linux, пожалуйста запускайте программу в среде Linux, через терминал(тк для отображения используеться консольная команда)
-#include<time.h>
-#include<unistd.h>//для использования sleep
-#include<stdlib.h>
-#include"math.h"
-typedef struct object{
-    int id;
-    int cord[2]; //2 координаты X и Y
-    int **memory;// двумерный массив,хранящий историю перемещений
-}obj;
-void rando(obj * stek, int count_obj, int count_mov, int mash){ //рандомное перемещение
-    int direct, i, j;
-    for(j=0;j<count_obj;j++){
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "math.h"
+#include <unistd.h>
+using namespace std;
+class Coordinates
+{
+    private:
+        int cord_X;
+        int cord_Y;
+    public:
+        Coordinates(int x, int y){
+            cord_X = x;
+            cord_Y = y;
+        }
+        Coordinates(){
+		cord_X = -1;
+		cord_Y = -1;
+        }
+        void setCord(int x, int y){
+            cord_X = x;
+            cord_Y = y;
+        }
+        int getCord(int choice_cord){
+            if(choice_cord == 0){
+                return cord_X;
+            }
+            else if(choice_cord == 1){
+                return cord_Y;
+            }
+		else return -1;
+        }
+        ~Coordinates(){
+        }
+};
+class Object
+{
+    private:
+        int Id;
+        Coordinates cord;
+        Coordinates *memory;
+    public:
+        Object(int id, int x, int y){
+            Id = id;
+            cord.setCord(x,y);
+        }
+	    Object(){
+            Id = 0;
+            cord.setCord(-1,-1);
+	    }
+        void memoryInit(int count_mov){
+            memory = new Coordinates[count_mov];
+        }
+        int memoryGet(int n, int choice_cord){
+		if(choice_cord == 0){
+			return memory[n].getCord(0);
+		}
+		else if(choice_cord == 1){
+			return memory[n].getCord(1);
+		}
+		else return -1;
+        }
+        int getId(){
+            return Id;
+        }
+        void setId(int id){
+            Id = id;
+        }
+        void setCord(int x, int y, int i){
+            cord.setCord(x,y);
+            memory[i].setCord(x,y);
+        }
+        int getCord(int choice_cord){
+            if(choice_cord == 0){
+                return cord.getCord(0);
+            }
+            else if(choice_cord == 1){
+                return cord.getCord(1);
+            }
+		else return -1;
+        }
+        void move(int direct, int mash, int mov){
+            if(direct == 0){ //На месте
+                cord.setCord(cord.getCord(0), cord.getCord(1));
+            }
+            if(direct == 1){ //Вверх
+                cord.setCord(cord.getCord(0), cord.getCord(1)+mash);
+            }
+            if(direct == 2){//Влево
+                cord.setCord(cord.getCord(0)-mash, cord.getCord(1));
+            }
+            if(direct == 3){//Вправо
+                cord.setCord(cord.getCord(0)+mash, cord.getCord(1));
+            }
+            if(direct == 4){//Вниз
+                cord.setCord(cord.getCord(0), cord.getCord(1)-mash);
+            }
+            memory[mov] = cord;
+        }
+        int lengthWay(int count_mov, int mash){
+            return count_mov * mash;
+        }
+        ~Object(){
+		delete[] memory;
+        }
+};
+
+void rando(Object * stek, int count_obj, int count_mov, int mash){ //рандомное перемещение
+    int direct, i, obj;
+    for(obj=0;obj<count_obj;obj++){
         for(i=1;i<count_mov;i++){
             direct = rand()%5;
             if(direct == 0){ //На месте
-                stek[j].cord[0] = stek[j].cord[0];
-                stek[j].cord[1] = stek[j].cord[1];
+                stek[obj].move(0, mash, i);
             }
-            if(direct == 1 && stek[j].cord[1]+mash<1000){ //Вверх
-                stek[j].cord[1]+=mash;
+            if(direct == 1 && stek[obj].getCord(1)+mash<1000){ //Вверх
+                stek[obj].move(1, mash, i);
             }
-            if(direct == 2 && stek[j].cord[0]-mash>0){//Влево
-                stek[j].cord[0]-=mash;   
+            if(direct == 2 && stek[obj].getCord(0)-mash>0){//Влево
+                stek[obj].move(2, mash, i);
             }
-            if(direct == 3 && stek[j].cord[0]+mash<1000){//Вправо
-                stek[j].cord[0]+=mash;
+            if(direct == 3 && stek[obj].getCord(0)+mash<1000){//Вправо
+                stek[obj].move(3, mash, i);
             }
-            if(direct == 4 && stek[j].cord[1]-mash>0){//Вниз
-                stek[j].cord[1]-=mash;
+            if(direct == 4 && stek[obj].getCord(1)-mash>0){//Вниз
+                stek[obj].move(4, mash, i);
             }
-            stek[j].memory[i][0]=stek[j].cord[0];
-            stek[j].memory[i][1]=stek[j].cord[1];
+		else stek[obj].move(0, mash, i);
         }
     }
 }
 //заглушки
-void rand_way(obj * stek, int count_obj, int count_mov, int mash){
+void rand_way(Object * stek, int count_obj, int count_mov, int mash){
 }
-void Gaus(obj * stek, int count_obj, int count_mov, int mash){
+void Gaus(Object * stek, int count_obj, int count_mov, int mash){
 }
-void Chain(obj * stek, int count_obj, int count_mov, int mash){
+void Chain(Object * stek, int count_obj, int count_mov, int mash){
 }
-int find(int id1, int id2, obj * stek, int count_obj, int count_mov, int mash){//найти расстояние
-	int i, j,rast, a,b,d, x1,y1, x2,y2;
+int find(int id1, int id2, Object * stek, int count_obj, int count_mov, int mash){//найти расстояние
+	int i, j,rast, a,b,d, x1,y1, x2,y2, way1,way2;
 	for(i=0;i<count_obj;i++){
-		if(id1 == stek[i].id){ //находит координаты указанных объектов
-			x1=stek[i].cord[0];
-			y1=stek[i].cord[1];
+		if(id1 == stek[i].getId()){ //находит координаты указанных объектов
+			x1=stek[i].getCord(0);
+			y1=stek[i].getCord(1);
+			way1 = stek[i].lengthWay(count_mov, mash);
 		}
-		if(id2 == stek[i].id){
-			x2=stek[i].cord[0];
-			y2=stek[i].cord[1];
+		if(id2 == stek[i].getId()){
+			x2=stek[i].getCord(0);
+			y2=stek[i].getCord(1);
+			way2 = stek[i].lengthWay(count_mov, mash);
 		}
 	}
-	printf("\nКоординаты 1 точки (%d, %d)", x1, y1);
-	printf("\nКоординаты 2 точки (%d, %d)", x2, y2);
-	a=x2-x1;
-	b=y2-y1;
-	rast=sqrt(pow(a,2)+ pow(b,2));
+	cout << "\nКоординаты 1 точки " << x1<<", "<< y1<<")";
+	cout << "\nКоординаты 2 точки " << x2<<", "<< y2<<")";
+	cout << "\nПройденное расстояние 1 точки (" <<way1<<"метров"<<endl;
+	cout << "\nПройденное расстояние 2 точки (" <<way2<<"метров"<<endl;
+	rast=sqrt(pow((x2-x1),2)+ pow((y2-y1),2));
 	return rast;
 }
 int main(){
     srand(time(NULL));
     int i,j,objt,x,y, chek=0, count_obj, count_mov, id, mash, choice, mov;
-    obj *stek; //массив структур
-    printf("\nВведите масштаб(рекоммендуется 1 к 20) 1к : "); //выбор масштаба
-    scanf("%d", &mash);
-    printf("\nВведите количество объектов: ");
-    scanf("%d", &count_obj);
-    printf("\nВведите количество движений: ");
-    scanf("%d", &count_mov);
-    stek = (obj*)malloc(count_obj * sizeof(obj)); //выделение памяти для массива
+    cout << "\nВведите масштаб(рекоммендуется 1 к 20) 1к : ";
+    cin >> mash;
+    cout << "\nВведите количество объектов: ";
+    cin >> count_obj;
+    cout << "\nВведите количество движений: ";
+    cin >> count_mov;
+    Object *stek = new Object[count_obj]; //выделение памяти для массива
     for(i=0;i<count_obj;i++){
-        obj temp;
-        temp.id = 0; //id что бы был, потом заменим
-        temp.cord[0]=-1; //задаем изначальные координаты  что бы были
-        temp.cord[1]=-1;
-        temp.memory = (int**)malloc(count_mov * sizeof(int*));//выделяем память для массива memmory
-        for(j=0;j<count_mov;j++){
-            temp.memory[j] = (int*)malloc(sizeof(int) * 2);//выделяем память для хранения координат в массиве memory
-        }
-        stek[i]=temp;//заполняем массив
+        stek[i].memoryInit(count_mov);
     }
+
     for(i=0;i<count_obj;i++){
-        printf("\nВведите ID объекта №%d ", i+1);  // Пока что id нужно вводить вручную, проверка на одинаковые значения пока не предусмотренна 
-        scanf("\n%d", &id);
+        cout << "\nВведите ID объекта № " << i+1 <<" ";  // Пока что id нужно вводить вручную, проверка на одинаковые значения пока не предусмотренна 
+        cin >> id;
         chek = 0;
         for(j=0;j<count_obj;j++){
-            if(id == stek[j].id)chek=1;
+            if(id == stek[j].getId())chek=1;
         }
-        if(chek==0)stek[i].id = id;
+        if(chek==0)stek[i].setId(id);
         else{
-            printf("\nID должен отличаться");
+            cout << "\nID должен отличаться"<<endl;
             i--;
         }
     }
@@ -100,7 +190,7 @@ int main(){
         x = (rand()%(1000/mash))*mash;//масштаб. В данном случае x и y это машсштабируемые координаты для отображения на экране(в структуре они все еще 1к1)
         y = (rand()%(1000/mash))*mash;//масштаб
         for(j=0;j<count_obj;j++){//выдача объектам начальных координат, одинаковые координаты не проходят
-            if(x!=stek[j].cord[0] && y!=stek[j].cord[1]){
+            if(x!=stek[j].getCord(0) && y!=stek[j].getCord(1)){
                 chek=1;
             }
             else{
@@ -109,61 +199,52 @@ int main(){
             }
         }
         if(chek==1){
-            stek[i].cord[0] = x; //Начальные координаты
-            stek[i].cord[1] = y; //Начальные координаты
-            stek[i].memory[0][0]=x; //помещение их в память
-            stek[i].memory[0][1]=y; //помещение их в память
+            stek[i].setCord(x,y,0); //Начальные координаты
         }
         else i--;
     }
-    printf("\nВыберете вариант движения: 1-random, 2-Random Waypoint, 3-Gaussian-Markov, 4-Markov Chains >> "); //заготовка 
-    scanf("%d", &choice);
+    cout << "\nВыберете вариант движения: 1-random, 2-Random Waypoint, 3-Gaussian-Markov, 4-Markov Chains >> " <<endl; //заготовка 
+    cin >> choice;
     if(choice == 1) rando(stek,count_obj, count_mov, mash);
     else if(choice == 2) rand_way(stek,count_obj, count_mov, mash);//заготовка
     else if(choice == 3) Gaus(stek,count_obj, count_mov, mash);//заготовка
     else if(choice == 4) Chain(stek,count_obj, count_mov, mash);//заготовка
     //отрисовка поля и объектов с учетом масштаба
-    for(mov=0;mov<count_mov;mov++){
+	for(mov=0;mov<count_mov;mov++){
 		sleep(1); // задержка 1 секунда
         system("clear");
         for(i=0;i<(1000/mash);i++){
             for(j=0;j<(1000/mash);j++){
 		        chek=0;
                 for(objt=0;objt<count_obj;objt++){
-                    if(i==((stek[objt].memory[mov][1])/mash) && j == ((stek[objt].memory[mov][0])/mash)){ //если встречается координата объекта
-                        printf("0 ");
+                    if(i==((stek[objt].memoryGet(mov,1))/mash) && j == ((stek[objt].memoryGet(mov,0))/mash)){ //если встречается координата объекта
+                        cout << "0 ";
                         chek=1;
                     }
-                    else if ((i!=((stek[objt].memory[mov][1])/mash) || j != ((stek[objt].memory[mov][0])/mash)) && (chek == 0)) chek = 0;
+                    else if ((i!=((stek[objt].memoryGet(mov,1))/mash) || j != ((stek[objt].memoryGet(mov,0))/mash)) && (chek == 0)) chek = 0;
                 }
-                if(chek==0) printf("* "); //если объект не встретился
+                if(chek==0) cout << "* "; //если объект не встретился
 	    }
-        printf("\n");
+        cout <<"\n";
     	}
     }
 	choice = 0;
-	int id_1,id_2, rasst;
+    int id_1,id_2, rasst;
 	while(choice!=2){
-		printf("\n\nХотите узнать расстояние между точками?1-Da 2-Net : ");
-		scanf("%d", &choice);
+		cout<< "\n\nХотите узнать расстояние между точками?1-Da 2-Net : ";
+		cin >> choice;
 		if(choice == 1){
-			printf("\nВведите ID 1точки: ");
-			scanf("%d",&id_1);
-			printf("\nВведите ID 2точки: ");
-			scanf("%d",&id_2);
+			cout << ("\nВведите ID 1точки: ");
+			cin >> id_1;
+			cout << ("\nВведите ID 2точки: ");
+			cin >> id_2;
 			rasst=find(id_1,id_2,stek,count_obj, count_mov,mash);
-			printf("\n\nРасстояние между точками равно %d метров", rasst);
+			cout << "\n\nРасстояние между точками равно " << rasst<< "метров" << endl;
 		}
-		else if(choice == 2)printf("\nПрограмма завершена");
-		else printf("\nError:Неверная команда\n");
+		else if(choice == 2)cout << "\nПрограмма завершена";
+		else cout << "\nError:Неверная команда\n";
 	}
     //очищение памяти
-	for(objt=0;objt<count_obj;objt++){
-		for(j=0;j<count_mov;j++){
-			free(stek[objt].memory[j]);
-		}
-		free(stek[objt].memory);
-	}
-	free(stek);
+	delete[] stek;
 }
 
